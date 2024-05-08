c095484379c6a478f43d7080b2a25dbe18ca4e38
nomisvdp <nomis.vandeputte@gmail.com>
2024-05-08 17:48:34 +0200

    added some documentation

--- src/com/alchemy/Device.java
+++ src/com/alchemy/Device.java
@@ -1,27 +1,62 @@
 package com.alchemy;
 import java.util.ArrayList;
-public class Device {
+public abstract class Device {
+    /**********************************************************
+     * Variables
+     **********************************************************/
     protected AlchemicIngredient ingredient;
-    public void addIngredient(IngredientContainer container) throws Exception{
-        ingredient = container.getContents();
-        container.Destroy();
 
-    }
-    public static class DeviceFullException extends Exception{
-        public DeviceFullException(String e){
-            super(e);
-        }
-    }
-    public void react(){}
-
-    public IngredientContainer getContents(){
+    /**********************************************************
+     * Getters and Setters
+     **********************************************************/
+    /**
+     * remove the contents of the device and return the smallest possible container containing the contents of the device
+     * @return smallest possible container containing the contents of the device. If quantity is too large, biggest possible
+     * container is returned and rest of contents are destroyed.
+     */
+    public IngredientContainer getContents() {
         int size = ingredient.getQuantity(); //TODO: calculate minimal needed quantity
-        IngredientContainer container = new IngredientContainer(ingredient,size);
+        IngredientContainer container = new IngredientContainer(ingredient, size);
         ingredient = null;
         return container;
     }
 
-    public boolean canAddIngredient(AlchemicIngredient ingredient){
+    /**********************************************************
+     * Mutators
+     **********************************************************/
+    /**
+     * @param container the container containing the ingredient that has to be added to the device
+     * @throws Exception if the device is full, deviceFullException is thrown.
+     */
+    public void addIngredient(IngredientContainer container) throws Exception {
+        ingredient = container.getContents();
+        container.Destroy();
+
+    }
+
+    public void react() {
+    }
+
+    /**********************************************************
+     * Inspectors
+     **********************************************************/
+    /**
+     * @param ingredient Check if ingredient can be added to the device
+     * @return true
+     */
+    public boolean canAddIngredient(AlchemicIngredient ingredient) {
         return true;
+    }
+
+    /**********************************************************
+     * Exceptions
+     **********************************************************/
+    /**
+     * Exception for when a device is full
+     */
+    public static class DeviceFullException extends Exception {
+        public DeviceFullException(String e) {
+            super(e);
+        }
     }
 }
--- src/com/alchemy/Temperature.java
+++ src/com/alchemy/Temperature.java
@@ -1,13 +1,38 @@
 package com.alchemy;
 import java.util.ArrayList;
-
+/**
+ * Temperature - total programming
+ *
+ * class to keep track of temperatures, with methods to heat and cool.
+ *
+ * @author MiroVanHoef
+ * @author BenDeMets
+ * @author SimonVandeputte
+ * @version 1.0
+ */
 public class Temperature {
+    /**********************************************************
+     * Variables
+     **********************************************************/
     private Long hotness;
     private Long coldness;
     static Long temperatureLimit = 10000L;
+    /**********************************************************
+     * Constructors
+     **********************************************************/
     Temperature(Long hotness,Long coldness){
         this.hotness= hotness;
         this.coldness = coldness;
+    }
+    /**********************************************************
+     * Getters and Setters
+     **********************************************************/
+
+    public Long getHotness(){
+        return hotness;
+    }
+    public Long getColdness(){
+        return coldness;
     }
     public ArrayList<Long> getTemperature(){
     ArrayList<Long> list = new ArrayList<>();
@@ -15,12 +40,14 @@ public class Temperature {
     list.add(hotness);
     return list;
     }
-    public Long getHotness(){
-        return hotness;
-    }
-    public Long getColdness(){
-        return coldness;
-    }
+    /**********************************************************
+     * Manipulators
+     **********************************************************/
+    /**
+     * @post coldness goes down by "change" amount.If coldness is zero, hotness goes up for the rest of the amount.
+     * if the change is less than 0, temperature will be heated instead
+     * @param change the change in temperature
+     */
     public void heat(Long change){
         if(change <0){
             cool(change);
@@ -32,7 +59,11 @@ public class Temperature {
             hotness = newTemps.getLast();
         }
     }
-
+    /**
+     * @post hotness goes down by "change" amount.If hotness is 0, coldness goes up for the rest of the change
+     * if the change is less than 0, temperature will be cooled instead
+     * @param change the change in temperature
+     */
     public void cool(Long change){
         if(change<0){
             heat(change);
@@ -45,6 +76,16 @@ public class Temperature {
         }
 
     }
+
+    /**
+     * method that changes temperature
+     *
+     * @param change the total amount the temperature changes.
+     * @param higher the amount of the variable that decreases
+     * @param lower the amount of the variable that increases.
+     * @post lower will be lowered by change amount or until its zero, in which case higher will be raised by the rest of change
+     * @return an arraylist of the two variables that have been changed.
+     */
     private ArrayList<Long> changeTemp(Long change, Long higher,Long lower){
         if(lower >0){
             if(lower>change){
